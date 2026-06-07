9ddcf6cda243738549f95a769444f98a9df82ada
bohn13 <bod3171@gmail.com>
2026-06-07 03:02:30 +0200

    total pipeline assault

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 module "resource_group_storage" {
-  source = "app.terraform.io/bohn13/resource_group_storage/azurerm"
+  source  = "app.terraform.io/bohn13/resource_group_storage/azurerm"
   version = "1.0.0"
 }
--- modules/resource_group_storage/variables.tf
+++ modules/resource_group_storage/variables.tf
@@ -1,17 +1,17 @@
 variable "resource_group_name" {
   description = "The name of the resource group to create."
   type        = string
-  default    = "test-storage"
+  default     = "test-storage"
 }
 
 variable "location" {
-    description = "The location of the resource group."
-    type       = string
-    default    = "eastus"
+  description = "The location of the resource group."
+  type        = string
+  default     = "eastus"
 }
 
 variable "storage_account_name" {
-    description = "The name of the storage account to create."
-    type       = string
-    default    = "teststorageaccount"
+  description = "The name of the storage account to create."
+  type        = string
+  default     = "teststorageaccount"
 }
